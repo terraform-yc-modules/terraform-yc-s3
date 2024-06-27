51f318dc36bf9df38eb9d581148a8180976ab30e
Anton Patsev <patsev.anton@gmail.com>
2024-06-27 11:55:26 +0600

    Fix No valid credential sources found

--- examples/all-in-one/versions.tf
+++ examples/all-in-one/versions.tf
@@ -20,7 +20,10 @@ terraform {
 }
 
 provider "aws" {
-  skip_region_validation      = true
+  region = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
